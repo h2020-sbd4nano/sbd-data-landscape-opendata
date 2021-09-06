14f0a02408acb80478abbe1f18e62b178de878f3
Egon Willighagen <egon.willighagen@gmail.com>
2021-09-06 09:25:22 +0200

    Fetch the other three shapes too

--- fetchShapes.sh
+++ fetchShapes.sh
@@ -8,8 +8,14 @@ wget --header "Authorization: token ${GH_TOKEN}" -O dataset.shex https://raw.git
 wget --header "Authorization: token ${GH_TOKEN}" -O dataset.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/dataset.rq
 wget --header "Authorization: token ${GH_TOKEN}" -O dataset_count.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/dataset_count.rq
 
+wget --header "Authorization: token ${GH_TOKEN}" -O model.shex https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/model.shex
+wget --header "Authorization: token ${GH_TOKEN}" -O model.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/model.rq
 wget --header "Authorization: token ${GH_TOKEN}" -O model_count.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/model_count.rq
 
+wget --header "Authorization: token ${GH_TOKEN}" -O database.shex https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/database.shex
+wget --header "Authorization: token ${GH_TOKEN}" -O database.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/database.rq
 wget --header "Authorization: token ${GH_TOKEN}" -O database_count.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/database_count.rq
 
+wget --header "Authorization: token ${GH_TOKEN}" -O assertion.shex https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/assertion.shex
+wget --header "Authorization: token ${GH_TOKEN}" -O assertion.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/assertion.rq
 wget --header "Authorization: token ${GH_TOKEN}" -O assertion_count.rq https://raw.githubusercontent.com/h2020-sbd4nano/sbd-data-landscape/master/shapes/assertion_count.rq
